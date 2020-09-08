a778781ea75a79857a2d8f99ee4e488079d53d59
Abraham Yepremian <abeyepremian@gmail.com>
2020-09-08 02:05:54 -0700

    update google analytics id

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   copyright: '© All rights reserved.',
   disqusShortname: 'abeyep',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: 'UA-177429283-1',
   useKatex: false,
   menu: [
     {
