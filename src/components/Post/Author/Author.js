c256ae5b4178cec9b84507572d09e49da0e1d915
Abraham Yepremian <abeyepremian@gmail.com>
2020-09-10 00:56:34 -0700

    began adding tinyletter email form

--- src/components/Post/Author/Author.js
+++ src/components/Post/Author/Author.js
@@ -20,7 +20,21 @@ const Author = () => {
           <strong>{author.name}</strong> on LinkedIn
         </a>
       </p>
+      <form
+        action="https://tinyletter.com/abeyep" method="post"
+        target="popupwindow"
+        onsubmit="window.open('https://tinyletter.com/abeyep', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true">
+        <p>
+          <label for="tlemail">Enter your email address</label>
+        </p>
+        <p>
+          <input type="text" name="email" id="tlemail" />
+        </p>
+        <input type="hidden" value="1" name="embed" />
+        <input type="submit" value="Subscribe" />
+      </form>
     </div>
+
   );
 };
 
