cc8b1e07828b8aa717f40b4a876667dac3929a1f
Abraham Yepremian <abeyepremian@gmail.com>
2020-10-04 18:22:03 -0700

    allow hash autoscroll in work page

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -13,9 +13,10 @@ const Page = ({ title, children }: Props) => {
     const url = new URL(pageRef.current.baseURI);
     // prevent autoscrolling to articles when home page is opened directly
     const isBaseHomePage = url.pathname === '/' && url.hash.length === 0;
-    // support for table of contents in projects page
+    // support for table of contents in projects page and allow tag autoscrolling in work page
     const isTaggedProjectsPage = url.pathname.startsWith('/pages/projects', 0) && url.hash.length > 0;
-    if (!isBaseHomePage && !isTaggedProjectsPage) {
+    const isTaggedWorkPage = url.pathname.startsWith('/pages/work', 0) && url.hash.length > 0;
+    if (!isBaseHomePage && !isTaggedProjectsPage && !isTaggedWorkPage) {
       pageRef.current.scrollIntoView();
     }
   });
